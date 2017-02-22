f46cc5e735e1aec81835da52cf9f042c7083821b
subdiox <subdiox@gmail.com>
2017-02-22 21:49:18 +0900

    first commit

--- infinishared/Infinilist.h
+++ infinishared/Infinilist.h
@@ -3,16 +3,16 @@
 /*
  * Copyright (c) 2010-2014, Xuzz Productions, LLC
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice, this
  *    list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
  *    this list of conditions and the following disclaimer in the documentation
  *    and/or other materials provided with the distribution.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -134,7 +134,7 @@ __attribute__((unused)) static SBIconController *IFIconControllerSharedInstance(
 
 __attribute__((unused)) static SBIconView *IFIconViewForIcon(SBIcon *icon) {
     SBIconController *iconController = IFIconControllerSharedInstance();
-    if ([iconController respondToSelector:@selector(homescreenIconViewMap)]) {
+    if ([iconController respondsToSelector:@selector(homescreenIconViewMap)]) {
         SBIconViewMap *iconViewMap = [iconController homescreenIconViewMap];
         return [iconViewMap iconViewForIcon:icon];
     } else {
@@ -1029,4 +1029,3 @@ static id grabbedIcon = nil;
 /* }}} */
 
 %end
-
